c9aeaee895b4208094a496512d5ee27ce8ed03c5
_tango <shurinskiy@mail.ru>
2021-10-03 14:10:55 +0600

    ready

--- src/blocks/discover/discover.js
+++ src/blocks/discover/discover.js
@@ -6,7 +6,41 @@ import Swiper from 'swiper/core';
 		speed: 800,
 		centeredSlides: true,
 		spaceBetween: 40,
-		slidesPerView: 4.1
+		slidesPerView: 4.1,
+		breakpoints: {
+			1280: { 
+				slidesPerView: 4.0,
+				spaceBetween: 40,
+			},
+			1100: { 
+				slidesPerView: 3.9,
+				spaceBetween: 40,
+			},
+			960: { 
+				slidesPerView: 3.5,
+				spaceBetween: 40,
+			},
+			780 : {
+				slidesPerView: 3.0,
+				spaceBetween: 40,
+			},
+			640: { 
+				slidesPerView: 2.45,
+				spaceBetween: 40
+			},
+			480: { 
+				slidesPerView: 2.0,
+				spaceBetween: 30
+			},
+			360: { 
+				slidesPerView: 1.5,
+				spaceBetween: 30
+			},
+			0: { 
+				slidesPerView: 1.1,
+				spaceBetween: 80
+			}
+		}
 	});
 
 })();
